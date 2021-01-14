e89b65d871c4f504d4fabbbcd4c4be895af38a72
Airubby <ai_rubby@qq.com>
2021-01-14 18:41:28 +0800

    新增界面

---
+++ src/views/pages/sysconfig/typeconfig/staticattributegroup/component/add.vue
@@ -0,0 +1,67 @@
+<template>
+    <el-dialog title='新增/编辑' :visible.sync="dialogInfo.visible" width="900px" v-dialogDrag :close-on-click-modal="false" :append-to-body="true">
+        <el-scrollbar style="height:420px;" class="scrollbar">
+            <div class="app-container">
+                <el-form ref="ValidateForm" :model="initParams" :rules="rules" label-position="top">
+                    <el-row :gutter="30">
+                        <el-col :span="12">
+                            <el-form-item label='参数编码' prop="name">
+                                <el-input v-model="initParams.a"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label='参数名称' prop="name">
+                                <el-input v-model="initParams.b"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label='数据类型' prop="name">
+                                <el-input v-model="initParams.name"></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label='备注' prop="name">
+                                <el-input v-model="initParams.name"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                </el-form>
+            </div>
+        </el-scrollbar>
+        <dialog-btn :dialogInfo="dialogInfo" @dialogSure="dialogSure()"></dialog-btn>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+    created() {
+        Object.assign(this.initParams,this.dialogInfo.data);
+    },
+    mounted() {
+        
+    },
+    data(){
+        return{
+            initParams:{
+                name:'',
+                a:'',
+                b:''
+            },
+            rules: {
+                
+            },
+        }
+    },
+	methods: {
+        //保存的操作
+        dialogSure:function(){
+            this.$refs['ValidateForm'].validate((valid) => {
+                if(valid){ //验证通过
+                    this.$emit('backInfo')
+                }
+            })
+        },
+	},
+    props:['dialogInfo']
+}
+</script>
